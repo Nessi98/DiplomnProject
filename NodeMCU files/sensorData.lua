a350b7f454de7135ce6f06669af1158d8fadbe08
Nessy <vanesa.nessi98@gmail.com>
2018-03-04 18:35:56 +0200

    Added comments and fixed temp and hum calculation

--- NodeMCU files/sensorData.lua
+++ NodeMCU files/sensorData.lua
@@ -1,17 +1,22 @@
-    SDA_PIN = 2-- sda pin, GPIO4
-    SCL_PIN = 1 -- scl pin, GPIO5
+SDA_PIN = 2 -- sda pin, GPIO4
+SCL_PIN = 1 -- scl pin, GPIO5
 
-    si7021 = require("si7021")
-    si7021.init(SDA_PIN, SCL_PIN)
-    si7021.read(OSS)
-    Hum = si7021.getHumidity()
-    Temp = si7021.getTemperature()
-    -- pressure in differents units
-    print("Humidity: ".. (Hum / 100) .. "." .. (Hum % 100) .. " %")
+si7021 = require("si7021")
+si7021.init(SDA_PIN, SCL_PIN)   --  Setting the i2c pin of si7021
+si7021.read(OSS)
 
-    -- temperature in degrees Celsius
-    print("Temperature: "..(Temp / 100).."."..(Temp % 100).." deg C")
+Hum = si7021.getHumidity()      -- Get humidity from si7021
+Temp = si7021.getTemperature()  -- Get temperature from si7021
 
-    -- release module
-    si7021 = nil
-    package.loaded["si7021"]=nil
+Hum = Hum / 100 .. "." .. Hum % 100 .. " %"
+Temp = Temp / 100 .. "." .. (Temp % 100) .. " C"
+
+-- pressure in differents units
+print("Humidity: ".. Hum)
+
+-- temperature in degrees Celsius
+print("Temperature: ".. Temp)
+
+-- release module
+si7021 = nil
+package.loaded["si7021"]=nil
