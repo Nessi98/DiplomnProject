b421728a0ec0913d2889e3335490177395bc8665
Vanessa Stoynova <vanesa.nessi98@gmail.com>
2018-03-22 00:48:30 -0700

    Add files via upload

---
+++ Web Interface/app.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require 'mqtt'
+require "sinatra/reloader" if development?
+
+client = MQTT::Client.new
+client.host = '127.0.0.1'
+client.ssl = true
+client.username = 'mosquitto'
+client.pasword = 'password'
+client.connect()
+
+get '/' do
+    # load these from the db
+  @sensors = [
+    {
+      name: 'Living room',
+      temp: '24.5 °C',
+      hum: '41.5 %'
+    },
+    {
+      name: 'Bathroom',
+      temp: '24 °C',
+      hum: '50 %' 
+    },
+    {
+      name: 'Kitchen',
+      temp: '22 °C',
+      hum: '40.5 %'
+    },
+    {
+      name: 'Sensor 004',
+      temp: '22 °C',
+      hum: '40.5 %'
+    },
+  ]
+  erb :index
+end
+
+get '/statistics' do
+  erb :statistics
+end
+
+get '/config' do
+  # load these from the db
+  @sensors = [
+    {
+      name: 'Living room',
+      enabled: true,
+      op_mode: 'sensor'
+    },
+    {
+      name: 'Sensonr name2',
+      enabled: false,
+      op_mode: 'IDLE' 
+    },
+    {
+      name: 'Sensor name3',
+      enabled: true,
+      op_mode: 'relay'
+    },
+  ]
+  erb :config
+end
