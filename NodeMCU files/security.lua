407cb8b5aa0abeb0812aeedb298a54ef820f3f61
Vanessa Stoynova <vanesa.nessi98@gmail.com>
2018-04-02 16:00:44 +0300

    Added messageArrieved function
    The function handles the message from the Brocker

--- NodeMCU files/security.lua
+++ NodeMCU files/security.lua
@@ -1,14 +1,31 @@
 -- Global variables and parameters.
 nodeID = "001"	            -- a node identifier for this device
-tgtHost = "192.168.1.13"	-- target host (broker)
+sleepTime = 0               -- duration of the energy saving mode
+
+-- Variables for connection to the Brocker
+tgtHost = "192.168.1.12"	-- target host (broker)
 tgtPort = 1883			    -- target port (broker listening on)
 mqttUserID = "mosquitto"	-- account to use to log into the broker
 mqttPass = "password"		-- broker account password
-mqttTimeOut = 120		    -- connection timeout
-dataInt = 1			
+mqttTimeOut = 120		    -- connection timeout		
 messageReceive = 1          -- data transmission interval in seconds 
-configTopic = "/system_name/" .. nodeID .. "/" .. "config"   -- the MQTT topic queue to use 
+
+-- Topic variables
+configTopic = "/system_name/" .. nodeID .. "/" .. "config" 
 discoverTopic = "/system_name/" .. "discover"
+
+-- Op_Mode variables
+opMode = 0 
+modeIDLE = "IDLE"
+modeSensor = "Sensor"
+modeRelay = "Relay"
+modeSensorAndRelay = "Sensor and Relay"
+
+--Op_mode parameters
+IDLE = 1
+Senosr = 2
+Relay = 3
+SensorAndRelay = 4 
     
 -- Reconnect to MQTT when we receive an "offline" message.
   
@@ -38,13 +55,43 @@ function pubEvent(message, topic)
     print("Publishing to " .. topic .. ": " .. message)
     mqttBroker:publish(topic, message, 0, 0)  -- publish
 end
+
+function messageArrieved(client, topic, data)
+    print("Delivered message on topic" .. topic .. ":" ) 
+    
+    if data ~= nil then
+        print(data)
+        messageReceive = 0
+    end
+
+    sleepTime = string.match(data, "%d+")
+       
+    if string.find(data, modeIDLE) then
+        opMode = IDLE
+    elseif (string.find(data, modeSensor))
+    then
+        -- set op_Mode to sensor
+        opMode = Sensor
+    elseif (string.find(data, modeRelay)) 
+    then
+        -- set op_Mode to Relay
+        opMode = Relay
+        -- get the value for the Relay
+    elseif (string.find(data, modeSensorAndRelay)) 
+    then
+        -- set op_mode to Sensor and Relay
+        opMode = SensorAndRelay
+        -- get the value for the Relay
+    else
+        --nothing
+    end
+end
      
 -- makeConn() instantiates the MQTT control object, sets up callbacks,
--- connects to the broker, and then uses the timer to send sensor data.
 -- This is the "main" function in this library. This should be called 
 -- from init.lua (which runs on the ESP8266 at boot), but only after
 -- it's been vigorously debugged. 
-     
+  
 function makeConn()
     -- Instantiate a global MQTT client object
     print("Instantiating mqttBroker")
@@ -54,20 +101,17 @@ function makeConn()
     print("Setting up callbacks")
     mqttBroker:on("connect", function(client) print ("connected") end)
     mqttBroker:on("offline", reconn)
-    mqttBroker:on("message", function(client, topic, data) 
-        print(topic .. ":" ) 
-        if data ~= nil then
-            print(data)
-            messageReceive = 0
-        end
-    end)
+    
+    mqttBroker:on("message", messageArrieved)
     
     -- Connect to the Broker
     conn()
 end
 
 function main()    
-    repeat
+    while (messageReceive > 0)
+    do
         pubEvent(nodeID, discoverTopic)
-    until messageReceive ~= 0
+        messageReceive = 0
+    end
 end
