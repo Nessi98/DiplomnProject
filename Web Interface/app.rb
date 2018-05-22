63028dd43a8d285fea1dae7114a068a95d97f946
Nessy98 <vanesa.nessi98@gmail.com>
2018-05-22 13:20:04 +0000

    Working on statistics

--- Web Interface/app.rb
+++ Web Interface/app.rb
@@ -17,89 +17,119 @@ client.subscribe(serverTopic)
 get '/' do
   # load these from the db
   
-  client.publish(serverAction, 'Real Time', false, 1)
+	client.publish(serverAction, 'Real Time', false, 1)
 
-  serverTopic, message = client.get
-  data = message.split(",")
- 
-  count = 0 
-  sensorsCount = 0
+	serverTopic, message = client.get
+	data = message.split(",")
+	 
+	count = 0 
+	sensorsCount = 0
 
-  nameArr  = []
-  tempArr = []
-  humArr = []
+	nameArr  = []
+	tempArr = []
+	humArr = []
 
-  data.each do |d|
-  
-    case  
-    when count == 0
-	nameArr[sensorsCount] = d
-	count += 1
-    when count == 1
-	tempArr[sensorsCount] = d
-	count += 1
-    else
-	humArr[sensorsCount] = d
+	data.each do |d|
+	  
+		case  
+		when count == 0
+			nameArr[sensorsCount] = d
+			count += 1
+		when count == 1
+			tempArr[sensorsCount] = d
+			count += 1
+		else
+			humArr[sensorsCount] = d
+			count = 0
+			sensorsCount += 1
+		end
+	end
+	
 	count = 0
-	sensorsCount += 1
-    end
-  end
-  count = 0
 
-  @sensors = Array.new
-  while count < sensorsCount do
-    @sensors << { :name => nameArr[count], :temp => tempArr[count], :hum => humArr[count]}
-    count += 1
-  end
+	@sensors = Array.new
+	while count < sensorsCount do
+		@sensors << { :name => nameArr[count], :temp => tempArr[count], :hum => humArr[count]}
+		count += 1
+	end
 
-  erb :index
+	erb :index
 end
 
+# Sends message to the C client to get data about the senosr unit in the DB
 get '/statistics' do
-  client.publish(serverAction, "Statistics", false, 1)
-  erb :statistics
+	client.publish(serverAction, "Statistics", false, 1)
+	
+	serverTopic, message = client.get
+	data = message.split(',')
+	
+	data.each do |d|
+		puts d
+		#data.split(",").each do |d|
+		#end
+	end
+	
+	@sensorsData = [
+	{
+		sensorName: 'Kitchen',
+		sensorColor: 'blue',
+		
+		dataPoints: [{
+			temp: 15.7,
+			time: '9:35'
+		},
+		{
+			temp: 16.9,
+			time: '9:42'
+		},
+		]
+	},
+	]
+	
+	erb :statistics
 end
 
 get '/config' do
-  # load these from the db
-  client.publish(serverAction, 'Config', false, 1)
+	# Send message to the C client to get information for the sensors
+	client.publish(serverAction, 'Config', false, 1)
 
-  serverTopic, message = client.get
- 
-  counter = 0
-  sensorCount = 0
+	serverTopic, message = client.get
+	 
+	counter = 0
+	sensorCount = 0
 
-  nameArr = []
-  opModeArr = []
-  enabledArr = []
+	nameArr = []
+	opModeArr = []
+	enabledArr = []
 
-  data = message.split(",")
-  
-  data.each do |d|
+	data = message.split(",")
+	  
+	data.each do |d|
 
-    case counter
-    when 0
-      nameArr[sensorCount] = d
-      counter += 1
-    else
-      if d.include? "IDLE"
-	enabledArr[sensorCount] = false
-      else
-	enabledArr[sensorCount] = true
-      end
-      opModeArr[sensorCount] = d
-      sensorCount += 1
-      counter = 0
-    end
-  end
+		case counter
+		when 0
+			nameArr[sensorCount] = d
+			counter += 1
+		else
+			if d.include? "IDLE"
+				enabledArr[sensorCount] = false
+			else
+				enabledArr[sensorCount] = true
+			end
+			
+			opModeArr[sensorCount] = d
+			sensorCount += 1
+			counter = 0
+		end
+	end
 
-  counter = 0
+	counter = 0
 
-  @sensors = Array.new
-  while counter < sensorCount do
-    @sensors << { :name => nameArr[counter], :enabled => enabledArr[counter], :op_Mode => opModeArr[counter]}
-    counter += 1
-  end
+	@sensors = Array.new
+	while counter < sensorCount do
+		@sensors << { :name => nameArr[counter], :enabled => enabledArr[counter], :op_Mode => opModeArr[counter]}
+		counter += 1
+	end
 
-   erb :config
+	erb :config
 end
