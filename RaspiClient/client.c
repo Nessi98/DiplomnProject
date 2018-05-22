6b325ccac6c9f1639efb42d4fe81244362f6aa07
Nessy98 <vanesa.nessi98@gmail.com>
2018-05-22 08:39:56 +0000

    working on config

--- RaspiClient/client.c
+++ RaspiClient/client.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,17 +12,20 @@
 #define CLIENTID    "raspi"
 #define PAYLOAD     "001"
 #define QOS         2	
-#define TIMEOUT     10000L
+#define TIMEOUT     1000L
 
-// Message headers
-#define SERVER		"/system_name/server"
-#define SERVERACK	"/system_name/server_ack"
-#define CONFIG		"/config"
-#define DATA		"/data"
-#define ACKNOWLEDGE "/config_ack"
-#define SYSTEM		"/system_name/"
-#define DISCOVER	"/system_name/discover"
-#define IDLEMESSAGE "op_Mode: IDLE; sleep_time: 50000"
+// Topic headers
+#define SERVER			"/system_name/server"
+#define SERVERACTION	"/system_name/server_action"
+#define CONFIG			"/config"
+#define DATA			"/data"
+#define ACKNOWLEDGE 	"/config_ack"
+#define SYSTEM			"/system_name/"
+#define DISCOVER		"/system_name/discover"
+
+#define IDLEMESSAGE 	"op_Mode: IDLE; sleep_time: 50000"
+#define REALTIMEMESSAGE "Real Time"
+#define CONFIGMESSAGE	"Config"
 
 #define QUERY "SELECT temp, hum FROM Data Where unitID = %d ORDER BY time DESC LIMIT 1"
 
@@ -29,9 +33,217 @@ static MQTTClient client;
 
 volatile MQTTClient_deliveryToken deliveredtoken;
 
+void loadDataToDB(char* dataMessage);
+void loadDataToServer(char * serverMessage);
+
+void connlost(void *context, char *cause);
+void subscribeForSensorUnit(char* discover);
+void publishMessage(char* message, char* topic);
+void delivered(void *context, MQTTClient_deliveryToken dt);
+int messageArrived(void *context, char *topicName, int topicLen, MQTTClient_message *message); 
+
+int main(int argc, char* argv[]){
+	
+	int rc;
+	char* username = "mosquitto"; 
+	char* password = "password";
+
+	
+	MQTTClient_deliveryToken token;
+	MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+	
+	conn_opts.keepAliveInterval = 20;
+	conn_opts.cleansession = 1;
+	conn_opts.username = username;
+	conn_opts.password = password;
+
+	MQTTClient_create(&client, BROKER, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+	
+	MQTTClient_setCallbacks(client, NULL, connlost, messageArrived, delivered);
+
+    if((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS){
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+	
+	printf("Successful conntection with the Brocker.\n");
+	
+	
+	rc = MQTTClient_subscribe(client, DISCOVER, QOS);
+	if(rc == 0){
+		printf("Subscribed for topic %s successful\n", DISCOVER);
+	}
+	
+	rc = MQTTClient_subscribe(client, SERVERACTION, QOS);
+	if(rc == 0){
+		printf("Subscribed for topic %s successful\n", SERVER);
+	}
+    
+	while(1){}
+	
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+	 
+	return rc;
+}
+
+void connlost(void *context, char *cause){
+    printf("\nConnection lost\n");
+    printf("     cause: %s\n", cause);
+}
+
 void delivered(void *context, MQTTClient_deliveryToken dt){
     printf("Message with token value %d delivery confirmed\n", dt);
     deliveredtoken =  dt;
+}
+
+int messageArrived(void *context, char *topicName, int topicLen, MQTTClient_message *message){
+    int count;
+    char* payloadptr;
+
+    printf("Message arrived\n");
+    printf("    topic: %s\n", topicName);
+    printf("	message: ");
+    payloadptr = message->payload;
+	
+    for(count = 0; count < message->payloadlen; count ++){
+        putchar(*payloadptr ++);
+    }
+	
+	putchar('\n');
+	
+	if(strstr(topicName, DISCOVER) != NULL){
+		//check if the arrieved discover already exist in the db
+		// if not wirte it to the db and subscribe
+		subscribeForSensorUnit(message->payload);
+	}else if(strstr(topicName, DATA) != NULL){
+		loadDataToDB(message->payload);
+	}else if(strstr(topicName, ACKNOWLEDGE) != NULL){
+		
+	}else if(strstr(topicName, SERVERACTION) != NULL){
+		loadDataToServer(message->payload);
+	}
+	
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+	
+	
+    return 1;
+}
+
+void loadDataToServer(char* serverMessage){
+	
+	sqlite3 *db;
+	sqlite3_stmt *stmt, *stmt2;
+	
+	int size = 1;
+	
+	char* comma = ",";
+	char* message = (char*) malloc (size);
+	
+	int len;
+	int flag = 0;
+	int count;
+	int rc = sqlite3_open("automation.db", &db);
+	
+	if(rc){
+		printf("Failed to open\n");
+		//return -1;
+	}
+	
+	printf("Performing query...\n");
+	if(strstr(serverMessage, REALTIMEMESSAGE) != NULL){
+		sqlite3_prepare_v2(db, "SELECT name, id from SensorUnit WHERE opMode != 'IDLE'", -1, &stmt, NULL);
+	}else if(strstr(serverMessage, CONFIGMESSAGE) != NULL){
+		sqlite3_prepare_v2(db, "SELECT name, opMode from SensorUnit", -1, &stmt, NULL);
+		flag = 1;
+	}else{
+		int id = 2;
+		time_t t = time(NULL);
+		struct tm tm = *localtime(&t);
+	
+		char start[19];
+		char end[19];
+		sprintf(start, "%d/%d/%d 00:00:00", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+		sprintf(end, "%d/%d/%d 23:59:59", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday);
+
+		printf("now: %s\n", start);
+		
+		char query[255];
+		//sprintf(query, "SELECT temp, hum, time FROM Data WHERE unitID = %d AND time > %s AND time < %s", id, start, end);
+		sprintf(query, "SELECT temp, hum, time FROM Data WHERE unitID = %d", id);
+		printf("%s\n", query);
+		sqlite3_prepare_v2(db, query, -1, &stmt, NULL);
+	}
+		
+	printf("Got results:\n");
+	while (sqlite3_step(stmt) != SQLITE_DONE) {
+
+		int col_num = sqlite3_column_count(stmt);
+
+		for (count = 0; count < col_num; count ++){
+				
+			switch (sqlite3_column_type(stmt, count))
+			{
+			case(SQLITE3_TEXT):
+				
+				len = strlen(sqlite3_column_text(stmt, count)) + 1;
+				size += len;
+					
+				message = (char*) realloc (message, size);
+				memcpy(message + size - (len + 1), sqlite3_column_text(stmt, count), len);
+				memcpy(message + size - 2, comma, 2);
+				printf("Unit Name = %s\n", sqlite3_column_text(stmt, count));
+					
+				break;
+				
+			case(SQLITE_INTEGER): ;
+
+				char* statement = malloc (strlen(QUERY) + 10);
+				sprintf(statement, QUERY, sqlite3_column_int(stmt, count));
+					
+				printf("Statement = %s\n", statement);
+					
+				sqlite3_prepare_v2(db, statement, -1, &stmt2, NULL);
+					
+				while(sqlite3_step(stmt2) != SQLITE_DONE) {
+						
+					int tempLen = strlen(sqlite3_column_text(stmt2, 0)); 
+					int humLen = strlen(sqlite3_column_text(stmt2, 1));
+					
+					size += tempLen + humLen + 2 * strlen(comma);
+					message = (char*) realloc (message, size);
+						
+					memcpy(message + size - (tempLen + humLen + 3), sqlite3_column_text(stmt2, 0), tempLen); 
+					memcpy(message + size - (humLen + 3), comma, 1);
+					memcpy(message + size - (humLen + 2), sqlite3_column_text(stmt2, 1), humLen); 
+					memcpy(message + size - 2, comma, 2); 
+						
+					printf("Temp = %s\n", sqlite3_column_text(stmt2, 0));
+					printf("Hum = %s\n", sqlite3_column_text(stmt2, 1));
+				}
+				
+				free(statement);
+				
+				printf("Unit id = %d\n", sqlite3_column_int(stmt, count));
+				break;
+			}
+		printf("Message = %s\n", message);
+		}
+	}
+	sqlite3_finalize(stmt);
+	
+	publishMessage(message, SERVER);
+	
+	free(message);
+	sqlite3_close(db);
+}
+
+void loadDataToDB(char* dataMessage){
+	// load sensor data to the db
 }
 
 void publishMessage(char* message, char* topic){
@@ -49,8 +261,8 @@ void publishMessage(char* message, char* topic){
 	MQTTClient_publishMessage(client, topic, &pubmsg, &token);
     printf("Waiting for up to %d seconds for publication of %s\n"
             "on topic %s for client with ClientID: %s\n",
-            (int)(TIMEOUT/500), pubmsg.payload, topic, CLIENTID);
-    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+            (int)(TIMEOUT/700), pubmsg.payload, topic, CLIENTID);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT/700);
 	
 	printf("Message with delivery token %d delivered\n", token);
 }
@@ -98,178 +310,4 @@ void subscribeForSensorUnit(char* discover){
 	publishMessage(IDLEMESSAGE, configTopic);
 	
 	free(configTopic);
-}
-
-void loadDataToDB(char* dataMessage){
-	// load sensor data to the db
-}
-
-
-void loadDataToServer(){
-	
-	sqlite3 *db;
-	sqlite3_stmt *stmt, *stmt2;
-	
-	int size = 1;
-	
-	char* comma = ",";
-	char* message = (char*) malloc (size);
-	
-	int len;
-	int count;
-	int rc = sqlite3_open("automation.db", &db);
-	
-	if(rc){
-		printf("Failed to open\n");
-	}else{
-		printf("Performing query...\n");
-		sqlite3_prepare_v2(db, "SELECT name, id from SensorUnit WHERE opMode != 'IDLE'", -1, &stmt, NULL);
-		
-		printf("Got results:\n");
-		while (sqlite3_step(stmt) != SQLITE_DONE) {
-
-			int col_num = sqlite3_column_count(stmt);
-
-			for (count = 0; count < col_num; count ++){
-				
-				switch (sqlite3_column_type(stmt, count))
-				{
-				case(SQLITE3_TEXT):
-				
-					len = strlen(sqlite3_column_text(stmt, count)) + 1;
-					size += len;
-					
-					message = (char*) realloc (message, size);
-					memcpy(message + size - (len + 1), sqlite3_column_text(stmt, count), len);
-					memcpy(message + size - 2, comma, 2);
-					printf("Unit Name = %s\n", sqlite3_column_text(stmt, count));
-					
-					break;
-				
-				case(SQLITE_INTEGER): ;
-
-					char* statement = malloc (strlen(QUERY) + 10);
-					sprintf(statement, QUERY, sqlite3_column_int(stmt, count));
-					
-					printf("Statement = %s\n", statement);
-					
-					sqlite3_prepare_v2(db, statement, -1, &stmt2, NULL);
-					
-					while(sqlite3_step(stmt2) != SQLITE_DONE) {
-						
-						int tempLen = strlen(sqlite3_column_text(stmt2, 0)); 
-						int humLen = strlen(sqlite3_column_text(stmt2, 1));
-						
-						size += tempLen + humLen + 2 * strlen(comma);
-						message = (char*) realloc (message, size);
-						
-						memcpy(message + size - (tempLen + humLen + 3), sqlite3_column_text(stmt2, 0), tempLen); 
-						memcpy(message + size - (humLen + 3), comma, 1);
-						memcpy(message + size - (humLen + 2), sqlite3_column_text(stmt2, 1), humLen); 
-						memcpy(message + size - 2, comma, 2); 
-						
-						printf("Temp = %s\n", sqlite3_column_text(stmt2, 0));
-						printf("Hum = %s\n", sqlite3_column_text(stmt2, 1));
-					}
-					printf("Unit id = %d\n", sqlite3_column_int(stmt, count));
-					break;
-				}
-			printf("Message = %s\n", message);
-			}
-		}
-	}
-	sqlite3_finalize(stmt);
-	
-	publishMessage(message, SERVER);
-	
-	free(message);
-	sqlite3_close(db);
-}
-
-int messageArrived(void *context, char *topicName, int topicLen, MQTTClient_message *message){
-    int count;
-    char* payloadptr;
-
-    printf("Message arrived\n");
-    printf("    topic: %s\n", topicName);
-    printf("	message: ");
-    payloadptr = message->payload;
-	
-    for(count = 0; count < message->payloadlen; count ++){
-        putchar(*payloadptr ++);
-    }
-	
-	putchar('\n');
-	
-	if(strstr(topicName, DISCOVER) != NULL){
-		//check if the arrieved discover already exist in the db
-		// if not wirte it to the db and subscribe
-		subscribeForSensorUnit(message->payload);
-	}else if(strstr(topicName, DATA) != NULL){
-		loadDataToDB(message->payload);
-	}else if(strstr(topicName, ACKNOWLEDGE) != NULL){
-		
-	}else if(strstr(topicName, SERVERACK) != NULL){
-		loadDataToServer();
-	}
-	
-    MQTTClient_freeMessage(&message);
-    MQTTClient_free(topicName);
-	
-	
-    return 1;
-}
-
-void connlost(void *context, char *cause){
-    printf("\nConnection lost\n");
-    printf("     cause: %s\n", cause);
-}
-
-
-int main(int argc, char* argv[]){
-	
-	int rc;
-	char* username = "mosquitto"; 
-	char* password = "password";
-
-	
-	MQTTClient_deliveryToken token;
-	MQTTClient_message pubmsg = MQTTClient_message_initializer;
-    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-	
-	conn_opts.keepAliveInterval = 20;
-	conn_opts.cleansession = 1;
-	conn_opts.username = username;
-	conn_opts.password = password;
-
-	MQTTClient_create(&client, BROKER, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL);
-    conn_opts.keepAliveInterval = 20;
-    conn_opts.cleansession = 1;
-	
-	MQTTClient_setCallbacks(client, NULL, connlost, messageArrived, delivered);
-
-    if((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS){
-        printf("Failed to connect, return code %d\n", rc);
-        exit(-1);
-    }
-	
-	printf("Successful conntection with the Brocker.\n");
-	
-	
-	rc = MQTTClient_subscribe(client, DISCOVER, QOS);
-	if(rc == 0){
-		printf("Subscribed for topic %s successful\n", DISCOVER);
-	}
-	
-	rc = MQTTClient_subscribe(client, SERVERACK, QOS);
-	if(rc == 0){
-		printf("Subscribed for topic %s successful\n", SERVER);
-	}
-    
-	while(1){}
-	
-    MQTTClient_disconnect(client, 10000);
-    MQTTClient_destroy(&client);
-	 
-	return rc;
 }
--- Web Interface/app.rb
+++ Web Interface/app.rb
@@ -6,7 +6,7 @@ require 'sinatra/reloader' if development?
 set :bind, '0.0.0.0'
 
 serverTopic = '/system_name/server'
-serverAck = '/system_name/server_ack'
+serverAction = '/system_name/server_action'
 
 client = MQTT::Client.new(:host => '127.0.0.1', :username => 'mosquitto', :password => 'password',  :keep_alive => 120)
 client.connect
@@ -17,71 +17,89 @@ client.subscribe(serverTopic)
 get '/' do
   # load these from the db
   
-  client.publish(serverAck, 'Some text', false, 1)
+  client.publish(serverAction, 'Real Time', false, 1)
 
   serverTopic, message = client.get
   data = message.split(",")
  
   count = 0 
-  i = 0
+  sensorsCount = 0
 
-  names = []
-  temps = []
-  hums = []
+  nameArr  = []
+  tempArr = []
+  humArr = []
+
   data.each do |d|
   
     case  
     when count == 0
-	names[i] = d
-	puts "Array of names", names
+	nameArr[sensorsCount] = d
 	count += 1
     when count == 1
-	temps[i] = d
-	puts temps
+	tempArr[sensorsCount] = d
 	count += 1
     else
-	hums[i] = d
-	puts hums
+	humArr[sensorsCount] = d
 	count = 0
-	i += 1
+	sensorsCount += 1
     end
   end
   count = 0
 
   @sensors = Array.new
-  while count < i do
-    puts names[count]
-    @sensors << { :name => names[count], :temp => temps[count], :hum => hums[count]}
+  while count < sensorsCount do
+    @sensors << { :name => nameArr[count], :temp => tempArr[count], :hum => humArr[count]}
     count += 1
   end
-
-  puts @sensors[0]
 
   erb :index
 end
 
 get '/statistics' do
+  client.publish(serverAction, "Statistics", false, 1)
   erb :statistics
 end
 
 get '/config' do
   # load these from the db
-  @sensors = [
-    {
-      name: 'Living room',
-      enabled: true,
-      op_mode: 'sensor'
-    },
-    {
-      name: 'Sensonr name2',
-      enabled: false,
-      op_mode: 'IDLE' 
-    },
-    {
-      name: 'Sensor name3',
-      enabled: true,
-      op_mode: 'relay'
-    },
-  ]
-  erb :config
+  client.publish(serverAction, 'Config', false, 1)
+
+  serverTopic, message = client.get
+ 
+  counter = 0
+  sensorCount = 0
+
+  nameArr = []
+  opModeArr = []
+  enabledArr = []
+
+  data = message.split(",")
+  
+  data.each do |d|
+
+    case counter
+    when 0
+      nameArr[sensorCount] = d
+      counter += 1
+    else
+      if d.include? "IDLE"
+	enabledArr[sensorCount] = false
+      else
+	enabledArr[sensorCount] = true
+      end
+      opModeArr[sensorCount] = d
+      sensorCount += 1
+      counter = 0
+    end
+  end
+
+  counter = 0
+
+  @sensors = Array.new
+  while counter < sensorCount do
+    @sensors << { :name => nameArr[counter], :enabled => enabledArr[counter], :op_Mode => opModeArr[counter]}
+    counter += 1
+  end
+
+   erb :config
 end
