5e6fd776ba91a52358ab80a4fe040f81f87418e8
Vanessa Stoynova <vanesa.nessi98@gmail.com>
2018-06-09 09:24:06 +0300

    fixed some functions

--- NodeMCU files/security.lua
+++ NodeMCU files/security.lua
@@ -1,13 +1,13 @@
 -- Global variables and parameters.
-nodeID = 4	            -- a node identifier for this device
-sleepTime = 1200        -- duration of the energy saving mode
+nodeID = 4                -- a node identifier for this device
+sleepTime = 12000         -- duration of the energy saving mode
 
 -- Variables for connection to the Brocker
-tgtHost = "192.168.1.12"	-- target host (broker)
-tgtPort = 1883			    -- target port (broker listening on)
-mqttUserID = "mosquitto"	-- account to use to log into the broker
-mqttPass = "password"		-- broker account password
-mqttTimeOut = 120		    -- connection timeout		
+tgtHost = "192.168.43.203"    -- target host (broker)
+tgtPort = 1883              -- target port (broker listening on)
+mqttUserID = "mosquitto"    -- account to use to log into the broker
+mqttPass = "password"       -- broker account password
+mqttTimeOut = 120           -- connection timeout       
 messageReceive = 1          -- data transmission interval in seconds 
 
 -- Topic variables
@@ -30,6 +30,9 @@ SensorAndRelay = 4
 
 SDA_PIN = 2 -- sda pin, GPIO4
 SCL_PIN = 1 -- scl pin, GPIO5
+
+Temp = 0
+relayValue = 24
     
 -- Reconnect to MQTT when we receive an "offline" message.
 function reconn()
@@ -40,12 +43,19 @@ end
 -- Establish a connection to the MQTT broker with the configured parameters.    
 function conn()
     print("Making connection to MQTT broker")
-    mqttBroker:connect(tgtHost, tgtPort, 0, function(client) print ("Connected")
-    mqttSub() end, function(client, reason) print("failed reason: "..reason) end)
+    
+    client:connect( "192.168.1.18" , 1883, 0, 
+        function(client) print("Connected") 
+      end,
+      
+      function(client, reason)
+        print("Failed readon: " .. reason)
+    end)
+    
 end
 
 function mqttSub()
-    mqttBroker:subscribe(configTopic, 0,
+    client:subscribe(configTopic, 0,
         function(conn) print("Subscribe for " .. configTopic .. " successful")
         main()
         end)
@@ -77,10 +87,22 @@ function getData()
     -- temperature in degrees Celsius
     print("Temperature: ".. Temp)
     data = Temp .. "," .. Hum
-    -- release module
+    -- release module 
     si7021 = nil
     package.loaded["si7021"] = nil
 
+    data = temp .. "," .. hum
+
+    pubEvent(data, configAckTopic)
+end
+
+function sensorAndRelayMode()
+    if Temp.tonumber(10) <= relayValue
+    then
+        --turn ON relay
+    else
+        -- turn OFF relay
+    end
 end
 
 function wake()
@@ -95,20 +117,18 @@ end
 
 function messageArrieved(client, topic, data)
     print("Delivered message on topic" .. topic .. ":" ) 
-    wake()
+
     if data ~= nil then
         print(data)
         messageReceive = 0
     end
-
-    sleepTime = string.match(data, "%d+")
        
     if string.find(data, modeIDLE) then
         opMode = IDLE
     elseif (string.find(data, modeSensor))
     then
-        -- set op_Mode to sensor
         opMode = Sensor
+        getData()
     elseif (string.find(data, modeRelay)) 
     then
         -- set op_Mode to Relay
@@ -118,13 +138,12 @@ function messageArrieved(client, topic, data)
     then
         -- set op_mode to Sensor and Relay
         opMode = SensorAndRelay
-        -- get the value for the Relay
-    else
-        --nothing
-    end
-    
-    print("Waked")
-    
+        getData()
+    elseif (string.find(data, "relayValue"))
+    then 
+        relayValue = string.match(data, "%d+")
+        sensroAndRelayMode()
+    end    
 end
  
 -- makeConn() instantiates the MQTT control object, sets up callbacks,
@@ -135,14 +154,14 @@ end
 function makeConn()
     -- Instantiate a global MQTT client object
     print("Instantiating mqttBroker")
-    mqttBroker = mqtt.Client(nodeId, mqttTimeOut, mqttUserID, mqttPass, 1)
+    client = mqtt.Client(nodeID, mqttTimeOut)
      
     -- Set up the event callbacks
     print("Setting up callbacks")
-    mqttBroker:on("connect", function(client) print ("connected") end)
-    mqttBroker:on("offline", reconn)
+    client:on("connect", function(client) print ("connected") end)
+    client:on("offline", reconn)
     
-    mqttBroker:on("message", messageArrieved)
+    client:on("message", messageArrieved)
     
     -- Connect to the Broker
     conn()
